262bae5b0c053d5114ab629cb97d4eee616d97a8
AdmiralWombat <dmhardzi@mtu.edu>
2026-02-11 22:12:31 -0600

    book list working with api calls

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/dto/SearchResponseDto.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/dto/SearchResponseDto.kt
@@ -4,6 +4,6 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class SearchResonseDto(
+data class SearchResponseDto(
     @SerialName("docs") val results: List<SearchedBookDto>
 )
