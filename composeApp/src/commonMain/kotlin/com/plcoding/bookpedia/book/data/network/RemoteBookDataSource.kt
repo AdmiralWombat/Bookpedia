0b5fbe44b49f3eaaa3cd07e005f2365b359e515b
AdmiralWombat <dmhardzi@mtu.edu>
2026-02-13 16:31:30 -0600

    add book synopsis getting

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/network/RemoteBookDataSource.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/network/RemoteBookDataSource.kt
@@ -1,5 +1,6 @@
 package com.plcoding.bookpedia.book.data.network
 
+import com.plcoding.bookpedia.book.data.dto.BookWorkDto
 import com.plcoding.bookpedia.book.data.dto.SearchResponseDto
 import com.plcoding.bookpedia.book.data.dto.SearchedBookDto
 import com.plcoding.bookpedia.core.domain.DataError
@@ -10,4 +11,6 @@ interface RemoteBookDataSource {
         query: String,
         resultLimit: Int? = null
     ): Result<SearchResponseDto, DataError.Remote>
+
+    suspend fun getBookDetails(bookWorkId: String): Result<BookWorkDto, DataError.Remote>
 }
